1b8cce20e14ce25422925aa1b336b559480b0d95
mahefasolofo <mahefa.rasolofonirina@gmail.com>
2022-04-26 08:49:31 +0300

    first commit

---
+++ src/projet1_annuaire/InscriptionController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet1_annuaire;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Inclusiv Academy 03
+ */
+public class InscriptionController implements Initializable {
+
+    @FXML
+    private HBox user_login;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
