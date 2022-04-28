c92d2d511edeec878c0c2465b50da5f618ccead6
Miora <m.andriamanjay@yahoo.com>
2022-04-28 14:08:19 +0300

    ajout données dans tableview

---
+++ src/projet1_annuaire/Etudiant.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet1_annuaire;
+
+/**
+ *
+ * @author Inclusiv
+ */
+public class Etudiant {
+    private String nom;
+    private String prenom;
+    private String sexe;
+    private String etablissement;
+    private String localisation;
+    private String secteur;
+    private String rentree;
+    private int id=0;
+
+    public Etudiant() {
+    }
+
+    
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getEtablissement() {
+        return etablissement;
+    }
+
+    public void setFormation(String etablissement) {
+        this.etablissement = etablissement;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+
+    public String getSecteur() {
+        return secteur;
+    }
+
+    public void setSecteur(String secteur) {
+        this.secteur = secteur;
+    }
+
+    public String getRentree() {
+        return rentree;
+    }
+
+    public void setRentree(String rentree) {
+        this.rentree = rentree;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
--- src/projet1_annuaire/HomeController.java
+++ src/projet1_annuaire/HomeController.java
@@ -5,13 +5,23 @@
  */
 package projet1_annuaire;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
+import static javafx.collections.FXCollections.observableArrayList;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -27,6 +37,26 @@ public class HomeController implements Initializable {
     private BorderPane borderpane;
     @FXML
     private AnchorPane anchorpane;
+    @FXML
+    private TableView<Etudiant> tableview;
+    @FXML
+    private TableColumn<Etudiant, Integer> colid;
+    @FXML
+    private TableColumn<Etudiant, String> colnom;
+    @FXML
+    private TableColumn<Etudiant, String> colprenom;
+    @FXML
+    private TableColumn<Etudiant, String> colsexe;
+    @FXML
+    private TableColumn<Etudiant, String> coletablissement;
+    @FXML
+    private TableColumn<Etudiant, String> colsecteur;
+    @FXML
+    private TableColumn<Etudiant, String> collocalisation;
+    @FXML
+    private TableColumn<Etudiant, String> colrentree;
+    
+    ObservableList<Etudiant> list = observableArrayList();
 
     /**
      * Initializes the controller class.
@@ -34,6 +64,7 @@ public class HomeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        tableset();
     }    
     
     private void loadpage (String page){
@@ -61,4 +92,77 @@ public class HomeController implements Initializable {
     private void showStatistique(MouseEvent event) {
         loadpage("statistique");
     }
+    
+    private void tableset (){
+         String line;
+        int id = 1;
+        try{
+            // InputStream :  pour avoir le fichier .txt toujours accessible en chemin relatif
+            String file = "C:\\Users\\Inclusiv\\Documents\\PROJET 1\\ProjectGIT\\Projet1_annuaire\\src\\projet1_annuaire\\donnees_Projet.txt";
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),StandardCharsets.UTF_8));
+            System.out.println("File found");
+        
+            TableColumn rentree_Universitaire = new TableColumn("rentree");
+            TableColumn nom = new TableColumn("nom");
+            TableColumn prenom = new TableColumn("prenom");
+            TableColumn localisation = new TableColumn("localisation");
+            TableColumn rgp_formation = new TableColumn("etablissement");
+            TableColumn secteur = new TableColumn("secteur");
+            TableColumn sexe = new TableColumn("sexe");
+            
+           while((line = br.readLine()) != null)
+           
+           {
+                Etudiant etudiant = new Etudiant();
+                String[] provisoireArray = line.split(";");
+                
+                etudiant.setRentree(provisoireArray[0]);
+                etudiant.setLocalisation(provisoireArray[1]);
+                etudiant.setFormation(provisoireArray[2]);
+                etudiant.setSecteur(provisoireArray[3]);
+                etudiant.setSexe(provisoireArray[4]);
+                etudiant.setNom(provisoireArray[5]);
+                etudiant.setPrenom(provisoireArray[6]);
+                etudiant.setId(id);
+                id++;
+                list.add(etudiant);
+            }
+           colid.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, Integer>("id")
+            );
+            
+            colrentree.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("rentree")
+            );
+            colnom.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("nom")
+            );
+            colprenom.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("prenom")
+            );
+            
+            collocalisation.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("localisation")
+            );
+            coletablissement.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("etablissement")
+            );
+            colsecteur.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("secteur")
+            );
+            colsexe.setCellValueFactory(
+                    new PropertyValueFactory<Etudiant, String>("sexe")
+            );
+            tableview.setItems(list);
+        }
+        catch (FileNotFoundException ex) {
+           
+        } catch (IOException ex) {
+           
+        }
+    
+    
+    
+    }
+    
 }
